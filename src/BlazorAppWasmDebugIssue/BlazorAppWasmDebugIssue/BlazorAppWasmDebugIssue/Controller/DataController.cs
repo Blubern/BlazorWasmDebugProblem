20de3f9cbfb8e2c8ac789236a6309baa00634379
Tobias Allweier <tobias.allweier@microsoft.com>
2024-03-01 17:56:41 +0100

    Adding a sample

---
+++ src/BlazorAppWasmDebugIssue/BlazorAppWasmDebugIssue/BlazorAppWasmDebugIssue/Controller/DataController.cs
@@ -0,0 +1,25 @@
+﻿using BlazorAppWasmDebugIssue.Client.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAppWasmDebugIssue.Controller
+{
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public async Task<IEnumerable<WeatherForecast>> GetAll()
+        {
+            // Simulate asynchronous loading to demonstrate a loading indicator
+            await Task.Delay(500);
+
+            var startDate = DateOnly.FromDateTime(DateTime.Now);
+            var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = summaries[Random.Shared.Next(summaries.Length)]
+            }).ToArray();
+        }
+    }
+}
---
+++ src/BlazorAppWasmDebugIssue/BlazorAppWasmDebugIssue/BlazorAppWasmDebugIssue/Program.cs
@@ -0,0 +1,55 @@
+using BlazorAppWasmDebugIssue.Client.Pages;
+using BlazorAppWasmDebugIssue.Components;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddControllers();
+
+// Add Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
+
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint.
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+});
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorAppWasmDebugIssue.Client._Imports).Assembly);
+
+app.MapControllers();
+
+app.Run();
